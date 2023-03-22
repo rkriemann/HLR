5aa224e31f16b0bd11f2ec95ad4886aa63cf7124
Ronald Kriemann <rok@mis.mpg.de>
2023-03-22 16:12:12 +0100

    added forgotten tensor-tbb program

---
+++ programs/tensor-tbb.cc
@@ -0,0 +1,15 @@
+//
+// Project     : HLR
+// Author      : Ronald Kriemann
+// Copyright   : Max Planck Institute MIS 2004-2021. All Rights Reserved.
+//
+
+#include "hlr/tbb/matrix.hh"
+#include "hlr/tbb/arith.hh"
+#include "hlr/tbb/tensor.hh"
+
+namespace          impl      = hlr::tbb;
+const std::string  impl_name = "tbb";
+
+#include "tensor.hh"
+#include "tbb.hh"
