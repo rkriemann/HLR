8d8cf9a3822737ff8754686eda57b94c7f19ea30
Ronald Kriemann <rok@mis.mpg.de>
2024-04-20 12:57:09 +0200

    compress/mixedprec: renaming and mulvec optimization

--- include/hlr/compress/mixedprec.hh
+++ include/hlr/compress/mixedprec.hh
@@ -105,19 +105,19 @@ public:
 //
 
 // finest precision
-using             mptype1_t = double;
+using             fp64_t    = double;
 
 // middle precision
-using             mptype2_t = float;
-constexpr double  mpprec2   = 6.0e-8;
+using             fp32_t    = float;
+constexpr double  fp32_prec = 6.0e-8;
 
 // coarsest precision
 #if HLR_HAS_FLOAT16
-using             mptype3_t = _Float16;
-constexpr double  mpprec3   = 4.9e-4;
+using             fp16_t    = _Float16;
+constexpr double  fp16_prec = 4.9e-4;
 #else
-using             mptype3_t = bf16;
-constexpr double  mpprec3   = 3.9e-3;
+using             fp16_t    = bf16;
+constexpr double  fp16_prec = 3.9e-3;
 #endif
 
 //////////////////////////////////////////////////////////////////////////////////////
@@ -147,7 +147,7 @@ compress_lr< double > ( const hlr::blas::matrix< double > &  U,
                         const hlr::blas::vector< double > &  S )
 {
     //
-    // determine corresponding parts for MP1, MP2, MP3
+    // determine corresponding parts for FP64, FP32, FP16
     //
 
     const size_t    n    = U.nrows();
@@ -169,67 +169,67 @@ compress_lr< double > ( const hlr::blas::matrix< double > &  U,
         return nprec;
     };
 
-    const uint32_t  n_mp3 = test_prec( mpprec3 );
-    const uint32_t  n_mp2 = test_prec( mpprec2 );
-    const uint32_t  n_mp1 = i+1;                   // remaining singular values
+    const uint32_t  n_fp16 = test_prec( fp16_prec );
+    const uint32_t  n_fp32 = test_prec( fp32_prec );
+    const uint32_t  n_fp64 = i+1;                   // remaining singular values
     size_t          s     = 0;
 
-    // std::cout << n_mp3 << " / " << n_mp2 << " / " << n_mp1 << std::endl;
+    // std::cout << n_fp16 << " / " << n_fp32 << " / " << n_fp64 << std::endl;
     
-    HLR_ASSERT( n_mp1 >= 0 );
-    HLR_ASSERT( n_mp3 + n_mp2 + n_mp1 == rank );
+    HLR_ASSERT( n_fp64 >= 0 );
+    HLR_ASSERT( n_fp16 + n_fp32 + n_fp64 == rank );
 
     //
     // copy into storage
     //
 
     const size_t  zsize = ( 3 * sizeof(uint32_t) +
-                            sizeof(mptype1_t) * n * n_mp1 +
-                            sizeof(mptype2_t) * n * n_mp2 +
-                            sizeof(mptype3_t) * n * n_mp3 );
+                            sizeof(fp64_t) * n * n_fp64 +
+                            sizeof(fp32_t) * n * n_fp32 +
+                            sizeof(fp16_t) * n * n_fp16 );
     zarray        zdata( zsize );
 
-    reinterpret_cast< uint32_t * >( zdata.data() )[0] = n_mp1;
-    reinterpret_cast< uint32_t * >( zdata.data() )[1] = n_mp2;
-    reinterpret_cast< uint32_t * >( zdata.data() )[2] = n_mp3;
+    reinterpret_cast< uint32_t * >( zdata.data() )[0] = n_fp64;
+    reinterpret_cast< uint32_t * >( zdata.data() )[1] = n_fp32;
+    reinterpret_cast< uint32_t * >( zdata.data() )[2] = n_fp16;
 
     uint32_t  k   = 0;
     size_t    pos = 3 * sizeof(uint32_t);
         
     {
-        auto    zptr = reinterpret_cast< mptype1_t * >( zdata.data() + pos );
+        auto    zptr = reinterpret_cast< fp64_t * >( zdata.data() + pos );
         size_t  zpos = 0;
 
-        for ( uint32_t  l = 0; l < n_mp1; ++l, ++k )
+        for ( uint32_t  l = 0; l < n_fp64; ++l, ++k )
         {
             for ( size_t  i = 0; i < n; ++i, ++zpos )
-                zptr[zpos] = mptype1_t( U(i,k) );
+                zptr[zpos] = fp64_t( U(i,k) );
         }// for
-        pos += n_mp1 * n * sizeof(mptype1_t);
+        pos += n_fp64 * n * sizeof(fp64_t);
     }
 
     {
-        auto    zptr = reinterpret_cast< mptype2_t * >( zdata.data() + pos );
+        auto    zptr = reinterpret_cast< fp32_t * >( zdata.data() + pos );
         size_t  zpos = 0;
 
-        for ( uint32_t  l = 0; l < n_mp2; ++l, ++k )
+        for ( uint32_t  l = 0; l < n_fp32; ++l, ++k )
         {
             for ( size_t  i = 0; i < n; ++i, ++zpos )
-                zptr[zpos] = mptype2_t( U(i,k) );
+                zptr[zpos] = fp32_t( U(i,k) );
         }// for
-        pos += n_mp2 * n * sizeof(mptype2_t);
+        pos += n_fp32 * n * sizeof(fp32_t);
     }
 
     {
-        auto    zptr = reinterpret_cast< mptype3_t * >( zdata.data() + pos );
+        auto    zptr = reinterpret_cast< fp16_t * >( zdata.data() + pos );
         size_t  zpos = 0;
 
-        for ( uint32_t  l = 0; l < n_mp3; ++l, ++k )
+        for ( uint32_t  l = 0; l < n_fp16; ++l, ++k )
         {
             for ( size_t  i = 0; i < n; ++i, ++zpos )
-                zptr[zpos] = mptype3_t( U(i,k) );
+                zptr[zpos] = fp16_t( U(i,k) );
         }// for
-        pos += n_mp3 * n * sizeof(mptype3_t);
+        pos += n_fp16 * n * sizeof(fp16_t);
     }
 
     HLR_ASSERT( k == rank );
@@ -260,62 +260,62 @@ compress_lr< std::complex< double > > ( const hlr::blas::matrix< std::complex< d
     //     size_t  pos_U = 0;
     //     size_t  pos_V = 0;
 
-    //     _mpdata.U1 = std::vector< mptype1_t >( 2 * n_mp1 * oU.nrows() );
-    //     _mpdata.V1 = std::vector< mptype1_t >( 2 * n_mp1 * oV.nrows() );
+    //     _mpdata.U1 = std::vector< fp64_t >( 2 * n_fp64 * oU.nrows() );
+    //     _mpdata.V1 = std::vector< fp64_t >( 2 * n_fp64 * oV.nrows() );
 
     //     for ( size_t  i = 0; i < _mpdata.U1.size(); i += 2 )
     //     {
     //         const auto  u_i = oU.data()[ pos_U++ ];
 
-    //         _mpdata.U1[i]   = mptype1_t( std::real( u_i ) );
-    //         _mpdata.U1[i+1] = mptype1_t( std::imag( u_i ) );
+    //         _mpdata.U1[i]   = fp64_t( std::real( u_i ) );
+    //         _mpdata.U1[i+1] = fp64_t( std::imag( u_i ) );
     //     }// for
 
     //     for ( size_t  i = 0; i < _mpdata.V1.size(); i += 2 )
     //     {
     //         const auto  v_i = oV.data()[ pos_V++ ];
 
-    //         _mpdata.V1[i]   = mptype1_t( std::real( v_i ) );
-    //         _mpdata.V1[i+1] = mptype1_t( std::imag( v_i ) );
+    //         _mpdata.V1[i]   = fp64_t( std::real( v_i ) );
+    //         _mpdata.V1[i+1] = fp64_t( std::imag( v_i ) );
     //     }// for
 
-    //     _mpdata.U2 = std::vector< mptype2_t >( 2 * n_mp2 * oU.nrows() );
-    //     _mpdata.V2 = std::vector< mptype2_t >( 2 * n_mp2 * oV.nrows() );
+    //     _mpdata.U2 = std::vector< fp32_t >( 2 * n_fp32 * oU.nrows() );
+    //     _mpdata.V2 = std::vector< fp32_t >( 2 * n_fp32 * oV.nrows() );
 
     //     for ( size_t  i = 0; i < _mpdata.U2.size(); i += 2 )
     //     {
     //         const auto  u_i = oU.data()[ pos_U++ ];
 
-    //         _mpdata.U2[i]   = mptype2_t( std::real( u_i ) );
-    //         _mpdata.U2[i+1] = mptype2_t( std::imag( u_i ) );
+    //         _mpdata.U2[i]   = fp32_t( std::real( u_i ) );
+    //         _mpdata.U2[i+1] = fp32_t( std::imag( u_i ) );
     //     }// for
 
     //     for ( size_t  i = 0; i < _mpdata.V2.size(); i += 2 )
     //     {
     //         const auto  v_i = oV.data()[ pos_V++ ];
 
-    //         _mpdata.V2[i]   = mptype2_t( std::real( v_i ) );
-    //         _mpdata.V2[i+1] = mptype2_t( std::imag( v_i ) );
+    //         _mpdata.V2[i]   = fp32_t( std::real( v_i ) );
+    //         _mpdata.V2[i+1] = fp32_t( std::imag( v_i ) );
     //     }// for
 
 
-    //     _mpdata.U3 = std::vector< mptype3_t >( 2 * n_mp3 * oU.nrows() );
-    //     _mpdata.V3 = std::vector< mptype3_t >( 2 * n_mp3 * oV.nrows() );
+    //     _mpdata.U3 = std::vector< fp16_t >( 2 * n_fp16 * oU.nrows() );
+    //     _mpdata.V3 = std::vector< fp16_t >( 2 * n_fp16 * oV.nrows() );
             
     //     for ( size_t  i = 0; i < _mpdata.U3.size(); i += 2 )
     //     {
     //         const auto  u_i = oU.data()[ pos_U++ ];
 
-    //         _mpdata.U3[i]   = mptype3_t( std::real( u_i ) );
-    //         _mpdata.U3[i+1] = mptype3_t( std::imag( u_i ) );
+    //         _mpdata.U3[i]   = fp16_t( std::real( u_i ) );
+    //         _mpdata.U3[i+1] = fp16_t( std::imag( u_i ) );
     //     }// for
 
     //     for ( size_t  i = 0; i < _mpdata.V3.size(); i += 2 )
     //     {
     //         const auto  v_i = oV.data()[ pos_V++ ];
 
-    //         _mpdata.V3[i]   = mptype3_t( std::real( v_i ) );
-    //         _mpdata.V3[i+1] = mptype3_t( std::imag( v_i ) );
+    //         _mpdata.V3[i]   = fp16_t( std::real( v_i ) );
+    //         _mpdata.V3[i+1] = fp16_t( std::imag( v_i ) );
     //     }// for
     // }// if
 }
@@ -348,49 +348,49 @@ decompress_lr< double > ( const zarray &                 zdata,
 {
     const size_t    nrows = U.nrows();
     const uint32_t  rank  = U.ncols();
-    const uint32_t  n_mp1 = reinterpret_cast< const uint32_t * >( zdata.data() )[0];
-    const uint32_t  n_mp2 = reinterpret_cast< const uint32_t * >( zdata.data() )[1];
-    const uint32_t  n_mp3 = reinterpret_cast< const uint32_t * >( zdata.data() )[2];
+    const uint32_t  n_fp64 = reinterpret_cast< const uint32_t * >( zdata.data() )[0];
+    const uint32_t  n_fp32 = reinterpret_cast< const uint32_t * >( zdata.data() )[1];
+    const uint32_t  n_fp16 = reinterpret_cast< const uint32_t * >( zdata.data() )[2];
     size_t          pos   = 3 * sizeof(uint32_t);
     uint32_t        k     = 0;
     
-    if ( n_mp1 > 0 )
+    if ( n_fp64 > 0 )
     {
-        auto    zptr = reinterpret_cast< const mptype1_t * >( zdata.data() + pos );
+        auto    zptr = reinterpret_cast< const fp64_t * >( zdata.data() + pos );
         size_t  zpos = 0;
 
-        for ( uint32_t  l = 0; l < n_mp1; ++l, ++k )
+        for ( uint32_t  l = 0; l < n_fp64; ++l, ++k )
         {
             for ( size_t  i = 0; i < nrows; ++i, ++zpos )
                 U(i,k) = zptr[zpos];
         }// for
-        pos += n_mp1 * nrows * sizeof(mptype1_t);
+        pos += n_fp64 * nrows * sizeof(fp64_t);
     }// if
 
-    if ( n_mp2 > 0 )
+    if ( n_fp32 > 0 )
     {
-        auto    zptr = reinterpret_cast< const mptype2_t * >( zdata.data() + pos );
+        auto    zptr = reinterpret_cast< const fp32_t * >( zdata.data() + pos );
         size_t  zpos = 0;
 
-        for ( uint32_t  l = 0; l < n_mp2; ++l, ++k )
+        for ( uint32_t  l = 0; l < n_fp32; ++l, ++k )
         {
             for ( size_t  i = 0; i < nrows; ++i, ++zpos )
                 U(i,k) = zptr[zpos];
         }// for
-        pos += n_mp2 * nrows * sizeof(mptype2_t);
+        pos += n_fp32 * nrows * sizeof(fp32_t);
     }// if
 
-    if ( n_mp3 > 0 )
+    if ( n_fp16 > 0 )
     {
-        auto    zptr = reinterpret_cast< const mptype3_t * >( zdata.data() + pos );
+        auto    zptr = reinterpret_cast< const fp16_t * >( zdata.data() + pos );
         size_t  zpos = 0;
 
-        for ( uint32_t  l = 0; l < n_mp3; ++l, ++k )
+        for ( uint32_t  l = 0; l < n_fp16; ++l, ++k )
         {
             for ( size_t  i = 0; i < nrows; ++i, ++zpos )
                 U(i,k) = zptr[zpos];
         }// for
-        pos += n_mp3 * nrows * sizeof(mptype3_t);
+        pos += n_fp16 * nrows * sizeof(fp16_t);
     }// if
 
     HLR_ASSERT( k == rank );
@@ -414,11 +414,11 @@ decompress_lr< std::complex< double > > ( const zarray &                        
     HLR_ERROR( "todo" );
     
     // size_t          pos   = 0;
-    // const uint32_t  n_mp1 = _mpdata.U1.size() / (2 * dU.nrows());
-    // const uint32_t  n_mp2 = _mpdata.U2.size() / (2 * dU.nrows());
-    // const uint32_t  n_mp3 = _mpdata.U3.size() / (2 * dU.nrows());
+    // const uint32_t  n_fp64 = _mpdata.U1.size() / (2 * dU.nrows());
+    // const uint32_t  n_fp32 = _mpdata.U2.size() / (2 * dU.nrows());
+    // const uint32_t  n_fp16 = _mpdata.U3.size() / (2 * dU.nrows());
                 
-    // for ( uint32_t  k1 = 0; k1 < n_mp1; ++k1, ++k )
+    // for ( uint32_t  k1 = 0; k1 < n_fp64; ++k1, ++k )
     // {
     //     const auto  s_k = _S(k);
                     
@@ -427,7 +427,7 @@ decompress_lr< std::complex< double > > ( const zarray &                        
     // }// for
 
     // pos = 0;
-    // for ( uint32_t  k2 = 0; k2 < n_mp2; ++k2, ++k )
+    // for ( uint32_t  k2 = 0; k2 < n_fp32; ++k2, ++k )
     // {
     //     const auto  s_k = _S(k);
                     
@@ -436,7 +436,7 @@ decompress_lr< std::complex< double > > ( const zarray &                        
     // }// for
                 
     // pos = 0;
-    // for ( uint32_t  k3 = 0; k3 < n_mp3; ++k3, ++k )
+    // for ( uint32_t  k3 = 0; k3 < n_fp16; ++k3, ++k )
     // {
     //     const auto  s_k = _S(k);
                     
@@ -514,9 +514,9 @@ mulvec_lr ( const size_t     nrows,
             const value_t *  x,
             value_t *        y )
 {
-    const uint32_t  n_mp1 = reinterpret_cast< const uint32_t * >( zA.data() )[0];
-    const uint32_t  n_mp2 = reinterpret_cast< const uint32_t * >( zA.data() )[1];
-    const uint32_t  n_mp3 = reinterpret_cast< const uint32_t * >( zA.data() )[2];
+    const uint32_t  n_fp64 = reinterpret_cast< const uint32_t * >( zA.data() )[0];
+    const uint32_t  n_fp32 = reinterpret_cast< const uint32_t * >( zA.data() )[1];
+    const uint32_t  n_fp16 = reinterpret_cast< const uint32_t * >( zA.data() )[2];
     size_t          zpos  = 3 * sizeof(uint32_t);
     size_t          pos   = 0;
 
@@ -524,31 +524,60 @@ mulvec_lr ( const size_t     nrows,
     {
         case  apply_normal :
         {
-            if ( n_mp1 > 0 )
+            if ( n_fp64 > 0 )
             {
-                auto  zptr = reinterpret_cast< const mptype1_t * >( zA.data() + zpos );
+                auto  zptr = reinterpret_cast< const fp64_t * >( zA.data() + zpos );
                 
-                mulvec< value_t, mptype1_t >( nrows, n_mp1, op_A, alpha, zptr, x + pos, y );
-                zpos += n_mp1 * nrows * sizeof(mptype1_t);
-                pos  += n_mp1;
+                if constexpr ( std::is_same_v< fp64_t, value_t > )
+                {
+                    blas::gemv( 'N', nrows, n_fp64, alpha, zptr, nrows, x + pos, 1, fp64_t(1), y, 1 );
+                }// if
+                else
+                {
+                    mulvec< value_t, fp64_t >( nrows, n_fp64, op_A, alpha, zptr, x + pos, y );
+                }// else
+                
+                zpos += n_fp64 * nrows * sizeof(fp64_t);
+                pos  += n_fp64;
             }// if
 
-            if ( n_mp2 > 0 )
+            if ( n_fp32 > 0 )
             {
-                auto  zptr = reinterpret_cast< const mptype2_t * >( zA.data() + zpos );
+                auto  zptr = reinterpret_cast< const fp32_t * >( zA.data() + zpos );
                 
-                mulvec< value_t, mptype2_t >( nrows, n_mp2, op_A, alpha, zptr, x + pos, y );
-                zpos += n_mp2 * nrows * sizeof(mptype2_t);
-                pos  += n_mp2;
+                if constexpr ( std::is_same_v< fp64_t, value_t > )
+                {
+                    auto  tx = blas::vector< fp32_t >( n_fp32 );
+                    auto  ty = blas::vector< fp32_t >( nrows );
+
+                    for ( size_t  i = 0; i < n_fp32; ++i )
+                        tx(i) = *(x + pos + i);
+                    
+                    blas::gemv( 'N', nrows, n_fp32, alpha, zptr, nrows, tx.data(), 1, fp32_t(0), ty.data(), 1 );
+
+                    for ( size_t  i = 0; i < nrows; ++i )
+                        y[i] += ty(i);
+                }// if
+                else if constexpr ( std::is_same_v< fp32_t, value_t > )
+                {
+                    blas::gemv( 'N', nrows, n_fp32, alpha, zptr, nrows, x + pos, 1, fp32_t(1), y, 1 );
+                }// if
+                else
+                {
+                    mulvec< value_t, fp32_t >( nrows, n_fp32, op_A, alpha, zptr, x + pos, y );
+                }// else
+                
+                zpos += n_fp32 * nrows * sizeof(fp32_t);
+                pos  += n_fp32;
             }// if
 
-            if ( n_mp3 > 0 )
+            if ( n_fp16 > 0 )
             {
-                auto  zptr = reinterpret_cast< const mptype3_t * >( zA.data() + zpos );
+                auto  zptr = reinterpret_cast< const fp16_t * >( zA.data() + zpos );
                 
-                mulvec< value_t, mptype3_t >( nrows, n_mp3, op_A, alpha, zptr, x + pos, y );
-                zpos += n_mp3 * nrows * sizeof(mptype3_t);
-                pos  += n_mp3;
+                mulvec< value_t, fp16_t >( nrows, n_fp16, op_A, alpha, zptr, x + pos, y );
+                zpos += n_fp16 * nrows * sizeof(fp16_t);
+                pos  += n_fp16;
             }// if
         }// case
         break;
@@ -559,31 +588,31 @@ mulvec_lr ( const size_t     nrows,
 
         case  apply_adjoint :
         {
-            if ( n_mp1 > 0 )
+            if ( n_fp64 > 0 )
             {
-                auto  zptr = reinterpret_cast< const mptype1_t * >( zA.data() + zpos );
+                auto  zptr = reinterpret_cast< const fp64_t * >( zA.data() + zpos );
                 
-                mulvec< value_t, mptype1_t >( nrows, n_mp1, op_A, alpha, zptr, x, y + pos );
-                zpos += n_mp1 * nrows * sizeof(mptype1_t);
-                pos  += n_mp1;
+                mulvec< value_t, fp64_t >( nrows, n_fp64, op_A, alpha, zptr, x, y + pos );
+                zpos += n_fp64 * nrows * sizeof(fp64_t);
+                pos  += n_fp64;
             }// if
 
-            if ( n_mp2 > 0 )
+            if ( n_fp32 > 0 )
             {
-                auto  zptr = reinterpret_cast< const mptype2_t * >( zA.data() + zpos );
+                auto  zptr = reinterpret_cast< const fp32_t * >( zA.data() + zpos );
                 
-                mulvec< value_t, mptype2_t >( nrows, n_mp2, op_A, alpha, zptr, x, y + pos );
-                zpos += n_mp2 * nrows * sizeof(mptype2_t);
-                pos  += n_mp2;
+                mulvec< value_t, fp32_t >( nrows, n_fp32, op_A, alpha, zptr, x, y + pos );
+                zpos += n_fp32 * nrows * sizeof(fp32_t);
+                pos  += n_fp32;
             }// if
 
-            if ( n_mp3 > 0 )
+            if ( n_fp16 > 0 )
             {
-                auto  zptr = reinterpret_cast< const mptype3_t * >( zA.data() + zpos );
+                auto  zptr = reinterpret_cast< const fp16_t * >( zA.data() + zpos );
                 
-                mulvec< value_t, mptype3_t >( nrows, n_mp3, op_A, alpha, zptr, x, y + pos );
-                zpos += n_mp3 * nrows * sizeof(mptype3_t);
-                pos  += n_mp3;
+                mulvec< value_t, fp16_t >( nrows, n_fp16, op_A, alpha, zptr, x, y + pos );
+                zpos += n_fp16 * nrows * sizeof(fp16_t);
+                pos  += n_fp16;
             }// if
         }// case
         break;
