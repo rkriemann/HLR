58590d6f6104849b67725b230f59cfbc1dc44939
Ronald Kriemann <rok@mis.mpg.de>
2024-12-11 07:48:19 +0100

    more tensor4 specific implementation

--- include/hlr/arith/detail/tensor4.hh
+++ include/hlr/arith/detail/tensor4.hh
@@ -14,17 +14,20 @@ tensor4< value_t >::unfold ( const uint  d ) const
     
     if ( d == 0 )
     {
-        auto    M   = matrix< value_t >( size(0), size(1) * size(2) );
+        auto    M   = matrix< value_t >( size(0), size(1) * size(2) * size(3) );
         size_t  col = 0;
 
-        for ( size_t  l = 0; l < size(2); ++l )
+        for ( size_t  i3 = 0; i3 < size(3); ++i3 )
         {
-            for ( size_t  j = 0; j < size(1); ++j )
+            for ( size_t  i2 = 0; i2 < size(2); ++i2 )
             {
-                auto  t_jl = fiber( d, j, l );
-                auto  m_c  = M.column( col++ );
+                for ( size_t  i1 = 0; i1 < size(1); ++i1 )
+                {
+                    auto  t_123 = fiber( d, i1, i2, i3 );
+                    auto  m_c   = M.column( col++ );
                     
-                blas::copy( t_jl, m_c );
+                    blas::copy( t_123, m_c );
+                }// for
             }// for
         }// for
             
@@ -32,35 +35,62 @@ tensor4< value_t >::unfold ( const uint  d ) const
     }// if
     else if ( d == 1 )
     {
-        auto    M   = matrix< value_t >( size(1), size(0) * size(2) );
+        auto    M   = matrix< value_t >( size(1), size(0) * size(2) * size(3) );
         size_t  col = 0;
 
-        for ( size_t  l = 0; l < size(2); ++l )
+        for ( size_t  i3 = 0; i3 < size(3); ++i3 )
         {
-            for ( size_t  i = 0; i < size(0); ++i )
+            for ( size_t  i2 = 0; i2 < size(2); ++i2 )
             {
-                auto  t_il = fiber( d, i, l );
-                auto  m_c  = M.column( col++ );
-                
-                blas::copy( t_il, m_c );
+                for ( size_t  i0 = 0; i0 < size(0); ++i0 )
+                {
+                    auto  t_023 = fiber( d, i0, i2, i3 );
+                    auto  m_c   = M.column( col++ );
+                    
+                    blas::copy( t_023, m_c );
+                }// for
             }// for
         }// for
         
         return M;
     }// if
-    else
+    else if ( d == 2 )
     {
-        auto    M   = matrix< value_t >( size(2), size(0) * size(1) );
+        auto    M   = matrix< value_t >( size(2), size(0) * size(1) * size(3) );
         size_t  col = 0;
         
-        for ( size_t  j = 0; j < size(1); ++j )
+        for ( size_t  i3 = 0; i3 < size(3); ++i3 )
         {
-            for ( size_t  i = 0; i < size(0); ++i )
+            for ( size_t  i1 = 0; i1 < size(1); ++i1 )
             {
-                auto  t_ij = fiber( d, i, j );
-                auto  m_c  = M.column( col++ );
+                for ( size_t  i0 = 0; i0 < size(0); ++i0 )
+                {
+                    auto  t_013 = fiber( d, i0, i1, i3 );
+                    auto  m_c   = M.column( col++ );
                 
-                blas::copy( t_ij, m_c );
+                    blas::copy( t_013, m_c );
+                }// for
+            }// for
+        }// for
+        
+        return M;
+    }// else
+    else // if ( d == 3 )
+    {
+        auto    M   = matrix< value_t >( size(3), size(0) * size(1) * size(2) );
+        size_t  col = 0;
+        
+        for ( size_t  i2 = 0; i2 < size(2); ++i2 )
+        {
+            for ( size_t  i1 = 0; i1 < size(1); ++i1 )
+            {
+                for ( size_t  i0 = 0; i0 < size(0); ++i0 )
+                {
+                    auto  t_012 = fiber( d, i0, i1, i2 );
+                    auto  m_c   = M.column( col++ );
+                
+                    blas::copy( t_012, m_c );
+                }// for
             }// for
         }// for
         
@@ -80,12 +110,14 @@ copy ( const tensor4< value_t > &  src,
 {
     HLR_DBG_ASSERT( ( src.size(0) == dest.size(0) ) &&
                     ( src.size(1) == dest.size(1) ) &&
-                    ( src.size(2) == dest.size(2) ) );
+                    ( src.size(2) == dest.size(2) ) &&
+                    ( src.size(3) == dest.size(3) ) );
 
-    for ( size_t  l = 0; l < src.size(2); l++ )
-        for ( size_t  j = 0; j < src.size(1); j++ )
-            for ( size_t  i = 0; i < src.size(0); i++ )
-                dest(i,j,l) = src(i,j,l);
+    for ( size_t  i3 = 0; i3 < src.size(3); i3++ )
+        for ( size_t  i2 = 0; i2 < src.size(2); i2++ )
+            for ( size_t  i1 = 0; i1 < src.size(1); i1++ )
+                for ( size_t  i0 = 0; i0 < src.size(0); i0++ )
+                    dest(i0,i1,i2,i3) = src(i0,i1,i2,i3);
 }
 using Hpro::BLAS::copy;
 
@@ -96,14 +128,16 @@ dot ( const tensor4< value_t > &  t1,
 {
     HLR_DBG_ASSERT( ( t1.size(0) == t2.size(0) ) &&
                     ( t1.size(1) == t2.size(1) ) &&
-                    ( t1.size(2) == t2.size(2) ) );
+                    ( t1.size(2) == t2.size(2) ) &&
+                    ( t1.size(3) == t2.size(3) ) );
 
     auto  d = value_t(0);
 
-    for ( size_t  l = 0; l < t1.size(2); l++ )
-        for ( size_t  j = 0; j < t1.size(1); j++ )
-            for ( size_t  i = 0; i < t1.size(0); i++ )
-                d += t1(i,j,l) * t2(i,j,l);
+    for ( size_t  i3 = 0; i3 < t1.size(3); i3++ )
+        for ( size_t  i2 = 0; i2 < t1.size(2); i2++ )
+            for ( size_t  i1 = 0; i1 < t1.size(1); i1++ )
+                for ( size_t  i0 = 0; i0 < t1.size(0); i0++ )
+                    d += t1(i0,i1,i2,i3) * t2(i0,i1,i2,i3);
 
     return d;
 }
@@ -123,10 +157,11 @@ max_abs_val ( const tensor4< value_t > &  t )
 {
     auto  v = real_type_t< value_t >(0);
 
-    for ( size_t  l = 0; l < t.size(2); l++ )
-        for ( size_t  j = 0; j < t.size(1); j++ )
-            for ( size_t  i = 0; i < t.size(0); i++ )
-                v += std::max( std::abs( t(i,j,l) ), v );
+    for ( size_t  i3 = 0; i3 < t.size(3); i3++ )
+        for ( size_t  i2 = 0; i2 < t.size(2); i2++ )
+            for ( size_t  i1 = 0; i1 < t.size(1); i1++ )
+                for ( size_t  i0 = 0; i0 < t.size(0); i0++ )
+                    v += std::max( std::abs( t(i0,i1,i2,i3) ), v );
 
     return v;
 }
@@ -139,17 +174,19 @@ template < typename alpha_t,
 requires ( std::convertible_to< alpha_t, value_t > )
 void
 add ( const alpha_t               alpha,
-      const tensor4< value_t > &  A,
-      tensor4< value_t > &        B )
+      const tensor4< value_t > &  t1,
+      tensor4< value_t > &        t2 )
 {
-    HLR_DBG_ASSERT( ( A.size(0) == B.size(0) ) &&
-                    ( A.size(1) == B.size(1) ) &&
-                    ( A.size(2) == B.size(2) ) );
+    HLR_DBG_ASSERT( ( t1.size(0) == t2.size(0) ) &&
+                    ( t1.size(1) == t2.size(1) ) &&
+                    ( t1.size(2) == t2.size(2) ) &&
+                    ( t1.size(3) == t2.size(3) ) );
     
-    for ( size_t  l = 0; l < A.size(2); l++ )
-        for ( size_t  j = 0; j < A.size(1); j++ )
-            for ( size_t  i = 0; i < A.size(0); i++ )
-                B(i,j,l) += value_t(alpha) * A(i,j,l);
+    for ( size_t  i3 = 0; i3 < t1.size(3); i3++ )
+        for ( size_t  i2 = 0; i2 < t1.size(2); i2++ )
+            for ( size_t  i1 = 0; i1 < t1.size(1); i1++ )
+                for ( size_t  i0 = 0; i0 < t1.size(0); i0++ )
+                    t2(i0,i1,i2,i3) += value_t(alpha) * t1(i0,i1,i2,i3);
 }
 
 template < typename alpha_t,
@@ -159,18 +196,20 @@ requires ( std::convertible_to< alpha_t, value_t > &&
            std::convertible_to< beta_t, value_t > )
 void
 add ( const alpha_t               alpha,
-      const tensor4< value_t > &  A,
+      const tensor4< value_t > &  t1,
       const beta_t                beta,
-      tensor4< value_t > &        B )
+      tensor4< value_t > &        t2 )
 {
-    HLR_DBG_ASSERT( ( A.size(0) == B.size(0) ) &&
-                    ( A.size(1) == B.size(1) ) &&
-                    ( A.size(2) == B.size(2) ) );
+    HLR_DBG_ASSERT( ( t1.size(0) == t2.size(0) ) &&
+                    ( t1.size(1) == t2.size(1) ) &&
+                    ( t1.size(2) == t2.size(2) ) &&
+                    ( t1.size(3) == t2.size(3) ) );
     
-    for ( size_t  l = 0; l < A.size(2); l++ )
-        for ( size_t  j = 0; j < A.size(1); j++ )
-            for ( size_t  i = 0; i < A.size(0); i++ )
-                B(i,j,l) = value_t(alpha) * A(i,j,l) + value_t(beta) * B(i,j,l);
+    for ( size_t  i3 = 0; i3 < t1.size(3); i3++ )
+        for ( size_t  i2 = 0; i2 < t1.size(2); i2++ )
+            for ( size_t  i1 = 0; i1 < t1.size(1); i1++ )
+                for ( size_t  i0 = 0; i0 < t1.size(0); i0++ )
+                    t2(i0,i1,i2,i3) = value_t(alpha) * t1(i0,i1,i2,i3) + value_t(beta) * t2(i0,i1,i2,i3);
 }
 using Hpro::BLAS::add;
 
@@ -190,47 +229,6 @@ tensor_product ( const tensor4< value_t > &  X,
                                   ( mode == 1 ? M.nrows() : X.size(1) ),
                                   ( mode == 2 ? M.nrows() : X.size(2) ) );
 
-    #if 0
-
-    if ( mode == 0 )
-    {
-        for ( size_t  l = 0; l < X.size(2); ++l )
-        {
-            auto  Xl = X.slice( 2, l );
-            auto  Yl = Y.slice( 2, l );
-
-            prod( value_t(1), M, Xl, value_t(0), Yl );
-        }// for
-    }// if
-    else if ( mode == 1 )
-    {
-        for ( size_t  l = 0; l < X.size(2); ++l )
-        {
-            for ( size_t  i = 0; i < X.size(0); ++i )
-            {
-                auto  x_ij = X.fiber( mode, i, l );
-                auto  y_ij = Y.fiber( mode, i, l );
-
-                mulvec( M, x_ij, y_ij );
-            }// for
-        }// for
-    }// if
-    else if ( mode == 2 )
-    {
-        for ( size_t  j = 0; j < X.size(1); ++j )
-        {
-            for ( size_t  i = 0; i < X.size(0); ++i )
-            {
-                auto  x_ij = X.fiber( mode, i, j );
-                auto  y_ij = Y.fiber( mode, i, j );
-
-                mulvec( M, x_ij, y_ij );
-            }// for
-        }// for
-    }// if
-    
-    #else
-    
     if ( mode == 0 )
     {
         for ( size_t  l = 0; l < X.size(2); ++l )
@@ -270,28 +268,41 @@ tensor_product ( const tensor4< value_t > &  X,
             }// for
         }// for
     }// if
+    else // if ( mode == 3 )
+    {
+        for ( size_t  j = 0; j < X.size(1); ++j )
+        {
+            for ( size_t  i = 0; i < X.size(0); ++i )
+            {
+                auto  x_ij = X.fiber( mode, i, j );
+                auto  y_ij = Y.fiber( mode, i, j );
 
-    #endif
+                mulvec( M, x_ij, y_ij );
+            }// for
+        }// for
+    }// if
 
     return Y;
 }
 
 //
-// element-wise multiplication X2 := X1 * X2
+// element-wise multiplication t2 := t1 * t2
 //
 template < typename value_t >
 tensor4< value_t >
-hadamard_product ( const tensor4< value_t > &  X1,
-                   tensor4< value_t > &        X2 )
+hadamard_product ( const tensor4< value_t > &  t1,
+                   tensor4< value_t > &        t2 )
 {
-    HLR_ASSERT( ( X1.size(0) == X2.size(0) ) &&
-                ( X1.size(1) == X2.size(1) ) &&
-                ( X1.size(2) == X2.size(2) ) );
-
-    for ( size_t  l = 0; l < X1.size(2); l++ )
-        for ( size_t  j = 0; j < X1.size(1); j++ )
-            for ( size_t  i = 0; i < X2.size(0); i++ )
-                X2(i,j,l) *= X1(i,j,l);
+    HLR_DBG_ASSERT( ( t1.size(0) == t2.size(0) ) &&
+                    ( t1.size(1) == t2.size(1) ) &&
+                    ( t1.size(2) == t2.size(2) ) &&
+                    ( t1.size(3) == t2.size(3) ) );
+    
+    for ( size_t  i3 = 0; i3 < t1.size(3); i3++ )
+        for ( size_t  i2 = 0; i2 < t1.size(2); i2++ )
+            for ( size_t  i1 = 0; i1 < t1.size(1); i1++ )
+                for ( size_t  i0 = 0; i0 < t1.size(0); i0++ )
+                    t2(i0,i1,i2,i3) *= t1(i0,i1,i2,i3);
 }
 
 }}// namespace hlr::blas
