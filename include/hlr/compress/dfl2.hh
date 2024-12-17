5559719071de914d027cd019bcbcd478f8068191
Ronald Kriemann <rok@mis.mpg.de>
2024-12-17 16:02:29 +0100

    modified bitrates (min=1, aplr = direct)

--- include/hlr/compress/dfl2.hh
+++ include/hlr/compress/dfl2.hh
@@ -52,13 +52,20 @@ using FP64 = fp_info< double >;
 //
 //   |d_i - ~d_i| ≤ 2^(-m) ≤ ε with mantissa length m = ⌈-log₂ ε⌉
 //
-inline byte_t eps_to_rate      ( const double  eps ) { return std::max< double >( 0, std::ceil( -std::log2( eps ) ) ); }
-inline byte_t eps_to_rate_aplr ( const double  eps ) { return eps_to_rate( eps ) + 1; }
+inline byte_t eps_to_rate      ( const double  eps ) { return std::max< double >( 1, std::ceil( -std::log2( eps ) ) ); }
+inline byte_t eps_to_rate_aplr ( const double  eps ) { return eps_to_rate( eps ); }
 
 struct config
 {
     byte_t  bitrate;
 };
+
+inline
+std::ostream &
+operator << ( std::ostream &  os, const config &  conf )
+{
+    return os << "rate " << conf.bitrate;
+}
 
 // holds compressed data
 using  zarray = std::vector< byte_t >;
